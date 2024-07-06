b8d3c137c87e46bf3ce9f31a34fe83b30d48caf8
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-06 13:17:47 +0300

    fake commit

--- lib/features/login/presentation/widgets/login_body.dart
+++ lib/features/login/presentation/widgets/login_body.dart
@@ -12,7 +12,6 @@ import 'top_section_login.dart';
 
 class LoginBody extends StatelessWidget {
   const LoginBody({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
