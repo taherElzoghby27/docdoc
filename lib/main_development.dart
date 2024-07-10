d86a1b6ca665e2b2f5f88242c9f4a79084c5dd14
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-11 00:10:15 +0300

    -add configration to build gradle for flavors -add launch.json in .vscode folder -edit androidmanifist -add development & production flavors

---
+++ lib/main_development.dart
@@ -0,0 +1,26 @@
+import 'package:doc_doc/core/routing/app_routes.dart';
+import 'package:doc_doc/core/theming/colors.dart';
+import 'package:doc_doc/doc_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'core/di/dependency_injection.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  setupGetIt();
+  //to fix issue with text being hidden bug in flutter_screenutil in release mode
+  //await ScreenUtil.ensureScreenSize();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor:
+          ColorsManager.mainBlue, // Change this to your desired color
+    ),
+  );
+  runApp(
+    DocApp(
+      appRouter: AppRouter(),
+    ),
+  );
+}
