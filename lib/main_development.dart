36207fe75cedb7b90c30b353e9f17cda7210c0c6
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-11 13:34:28 +0300

    add configuration for ios with xcode

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,6 +3,7 @@ import 'package:doc_doc/core/theming/colors.dart';
 import 'package:doc_doc/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/di/dependency_injection.dart';
 
@@ -11,11 +12,12 @@ void main() async {
   // To fix texts being hidden bug in flutter_screenutil in release mode.
   setupGetIt();
   //to fix issue with text being hidden bug in flutter_screenutil in release mode
-  //await ScreenUtil.ensureScreenSize();
+  await ScreenUtil.ensureScreenSize();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor:
           ColorsManager.mainBlue, // Change this to your desired color
+      systemNavigationBarColor: Colors.orange,
     ),
   );
   runApp(
